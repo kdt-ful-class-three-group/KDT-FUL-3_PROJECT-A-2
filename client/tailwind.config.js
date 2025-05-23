ca2f57f2fc36dd32399f1673632d72f3432102e0
김재승 <jaes2878@gmail.com>
2025-05-23 13:00:44 +0900

    데이터베이스 인터페이스 작성 완료

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        black: "#313131",
+        "main-color-orange": "#FC4F00",
+        "main-color-2": "#1E3E62",
+        "main-color-3": "#0B192C",
+        "main-color-4": "#07111F",
+        buying: "#E23C4F",
+        sell: "#1A68CC",
+        border: "#D9D9D9",
+        background: "#F1F1F1",
+        input: "#BABCBD",
+      },
+    },
+  },
+  plugins: [],
+};
