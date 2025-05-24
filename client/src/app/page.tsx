fe8cc821e1d92f00922b5abe26feb9dda326cbb4
김재승 <jaes2878@gmail.com>
2025-05-24 16:59:39 +0900

    main페이지 컴포넌트 완료

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,13 @@
+import React from "react";
+import Nav from "@/components/Nav";
+import Title from "@/components/Title";
 export default function Home() {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="m-auto">
+      {/* body 컬러 변경해야함 */}
+      <Title title="한화이글스" />
+
+      <Nav />
+    </div>
+  );
 }
