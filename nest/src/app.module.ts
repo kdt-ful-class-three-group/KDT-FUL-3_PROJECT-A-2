2c81c38fc4c686e5fa1b59121647a4c6b7967f9e
윤종환 <yunjonghwan99@gmail.com>
2025-05-26 15:36:56 +0900

    Feat : History 모듈 및 컨트롤러 추가, AppModule에 History 모듈 추가

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { StockModule } from './stocks/stocks.module';
 import { AuthModule } from './auth/auth.module';
 import { BankModule } from './bank/bank.module';
+import { HistoryModule } from './history/history.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { BankModule } from './bank/bank.module';
     StockModule,
     AuthModule,
     BankModule,
+    HistoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
