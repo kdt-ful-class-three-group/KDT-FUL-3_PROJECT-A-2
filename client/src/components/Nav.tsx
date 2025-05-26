76ef8f13611236024822e49ac9616d706eceaa87
윤종환 <yunjonghwan99@gmail.com>
2025-05-26 11:06:26 +0900

    네비게이션 버튼 텍스트 색상 변경

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -3,29 +3,29 @@ export default function Nav() {
     <div className="absolute bottom-0 p-5 flex bg-[#1E3E62] w-full justify-between items-center">
       <button className="cursor-pointer flex flex-col items-center">
         <img src="./image/home.svg" alt="" />
-        <p className="mt-2">거래소</p>
+        <p className="mt-2 text-white">거래소</p>
       </button>
       <button className="cursor-pointer flex flex-col items-center">
         <img src="./image/bookmark.svg" alt="" />
-        <p className="mt-2">관심</p>
+        <p className="mt-2 text-white">관심</p>
       </button>
       <button className="cursor-pointer flex flex-col items-center">
         <img src="./image/investment.svg" alt="" />
-        <p className="mt-2">투자내역</p>
+        <p className="mt-2 text-white">투자내역</p>
       </button>
       <button className="cursor-pointer flex flex-col items-center">
         <img src="./image/bank.svg" alt="" />
-        <p className="mt-2">은행</p>
+        <p className="mt-2 text-white">은행</p>
       </button>
       <button className="cursor-pointer flex flex-col items-center">
         <img src="./image/mypage.svg" alt="" />
-        <p className="mt-2">마이페이지</p>
+        <p className="mt-2 text-white">마이페이지</p>
       </button>
       {/* 로그인 푤요하다면 사용 */}
-      <button className="cursor-pointer flex flex-col items-center">
+      {/* <button className="cursor-pointer flex flex-col items-center">
         <img src="./image/login.svg" alt="" />
-        <p className="mt-2">로그인</p>
-      </button>
+        <p className="mt-2 text-white">로그인</p>
+      </button> */}
     </div>
   );
 }
