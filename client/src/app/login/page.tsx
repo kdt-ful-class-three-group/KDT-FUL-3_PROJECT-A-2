d5efd25e1e2535ce71b51f33eb55fa5aa281ead6
윤종환 <yunjonghwan99@gmail.com>
2025-05-27 17:45:04 +0900

    Feat: 로그인 입력 필드와 버튼을 폼 태그로 감싸기

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -7,28 +7,34 @@ export default function LoginPage() {
       <div className="flex flex-col w-full max-w-xs mt-10 mb-10">
         <p className="text-3xl text-[#1E3E62]">안녕하세요.<br />모투샷입니다.</p>
       </div>
-      <div className="w-full max-w-xs mb-5">
-        <input
-          className="pl-2 rounded-lg border w-full py-2"
-          type="text"
-          placeholder="아이디"
-          name="userid"
-        />
-      </div>
-      <div className="w-full max-w-xs">
-        <input
-          className="pl-2 rounded-lg border w-full py-2"
-          type="password"
-          placeholder="비밀번호"
-          name="password"
-        />
-      </div>
-      <button
-        type="submit"
-        className="w-full max-w-xs mt-5 border-1 text-center bg-[#1E3E62] text-[#FFFFFF] p-2 rounded-lg"
+      <form
+        className="flex flex-col items-center w-full"
+        action="http://localhost:8000/users/login"
+        method="POST"
       >
-        로그인
-      </button>
+        <div className="w-full max-w-xs mb-5">
+          <input
+            className="pl-2 rounded-lg border w-full py-2"
+            type="text"
+            placeholder="아이디"
+            name="userid"
+          />
+        </div>
+        <div className="w-full max-w-xs">
+          <input
+            className="pl-2 rounded-lg border w-full py-2"
+            type="password"
+            placeholder="비밀번호"
+            name="password"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full max-w-xs mt-5 border-1 text-center bg-[#1E3E62] text-[#FFFFFF] p-2 rounded-lg"
+        >
+          로그인
+        </button>
+      </form>
       <div className="flex justify-between w-full max-w-xs mt-10 mb-10">
         <a href="">아이디 찾기</a>
         <a href="">비밀번호 찾기</a>
