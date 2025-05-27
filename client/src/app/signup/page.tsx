6523eefbb4f4d4a8add4344b713d7bc343259ca3
윤종환 <yunjonghwan99@gmail.com>
2025-05-27 12:06:04 +0900

    Style: 회원가입 페이지 아이디 및 비밀번호 입력 필드 레이아웃 수정

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -5,8 +5,8 @@ export default function SignupPage() {
   return (
     <div className="m-auto flex flex-col items-center w-full px-5">
       <Title title="회원가입" bookmark={false} dictionary={false} />
-      <div className="flex flex-col w-full max-w-xs mt-10">
-        <label className="text-[#FC4F00] mb-5">아이디</label>
+      <div className="flex flex-col w-full max-w-xs">
+        <label className="text-[#FC4F00] mb-3">아이디</label>
         <div className="flex w-full justify-around">
           <input
             className="pl-2 rounded-lg border"
@@ -20,6 +20,24 @@ export default function SignupPage() {
           >
             중복확인
           </button>
+        </div>
+      </div>
+      <div className="flex flex-col w-full max-w-xs mt-10">
+        <label className="text-[#FC4F00] mb-3">비밀번호</label>
+        <div className="w-full justify-around">
+          <input
+            className="pl-2 rounded-lg border py-2 w-full mb-3"
+            type="text"
+            placeholder="비밀번호"
+            name="userid"
+          />
+          <input
+            className="pl-2 rounded-lg border py-2 w-full mb-3"
+            type="text"
+            placeholder="비밀번호 확인"
+            name="userid"
+          />
+          <p className="text-[#1E3E62] text-[70%]">6~20자/영문 대문자, 소문자, 숫자, 특수문자 중 2가지 이상 조합</p>
         </div>
       </div>
     </div>
