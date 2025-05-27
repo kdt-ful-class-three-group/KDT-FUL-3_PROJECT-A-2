7dc34fa3380d03d6cea19708d3e77e82a93bbc2f
윤종환 <yunjonghwan99@gmail.com>
2025-05-27 12:16:57 +0900

    Style: 회원가입 페이지 아이디 및 비밀번호 입력 필드 스타일 수정 및 휴대폰 번호 입력 필드 추가

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -9,20 +9,20 @@ export default function SignupPage() {
         <label className="text-[#FC4F00] mb-3">아이디</label>
         <div className="flex w-full justify-around">
           <input
-            className="pl-2 rounded-lg border"
+            className="pl-2 rounded-lg border py-2"
             type="text"
             placeholder="아이디"
             name="userid"
           />
           <button
-            className="bg-[#E5E5E5] text-[#1E3E62] rounded-lg py-3 px-5"
+            className="bg-[#E5E5E5] text-[#1E3E62] rounded-lg px-2"
             type="button"
           >
             중복확인
           </button>
         </div>
       </div>
-      <div className="flex flex-col w-full max-w-xs mt-10">
+      <div className="flex flex-col w-full max-w-xs mt-5">
         <label className="text-[#FC4F00] mb-3">비밀번호</label>
         <div className="w-full justify-around">
           <input
@@ -38,6 +38,31 @@ export default function SignupPage() {
             name="userid"
           />
           <p className="text-[#1E3E62] text-[70%]">6~20자/영문 대문자, 소문자, 숫자, 특수문자 중 2가지 이상 조합</p>
+        </div>
+      </div>
+      <div className="flex flex-col w-full max-w-xs mt-5">
+        <label className="text-[#FC4F00] mb-3">휴대폰 번호</label>
+        <div className="flex w-full justify-around mb-3">
+          <input
+            className="pl-2 rounded-lg border py-2"
+            type="text"
+            placeholder="휴대폰 번호"
+            name="userid"
+          />
+          <button
+            className="bg-[#E5E5E5] text-[#1E3E62] rounded-lg"
+            type="button"
+          >
+            인증번호 받기
+          </button>
+        </div>
+        <div>
+          <input
+            className="pl-2 rounded-lg border py-2 w-full mb-3"
+            type="text"
+            placeholder="인증번호 입력"
+            name="userid"
+          />
         </div>
       </div>
     </div>
