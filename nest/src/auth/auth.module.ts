d5fecd7ebe255b46d9b6b805fba81a118c6c63f0
윤종환 <yunjonghwan99@gmail.com>
2025-05-30 15:06:01 +0900

    feat: 메일 전송 기능을 위한 MailerModule 추가

--- nest/src/auth/auth.module.ts
+++ nest/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+      defaults: {
+        from: '"Your Project" <yourproject.auth@gmail.com>',
+      },
+    }),
+  ],
 })
 export class AuthModule {}
