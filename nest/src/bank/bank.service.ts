2f7d71255353f76b848369984ce0db09c4ac0dcb
윤종환 <yunjonghwan99@gmail.com>
2025-06-02 11:54:17 +0900

    Refactor: 불필요한 주석 제거 및 코드 정리

--- nest/src/bank/bank.service.ts
+++ nest/src/bank/bank.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateBankDto } from './dto/create-bank.dto';
 import { pool } from 'src/DB/DB';
 
 @Injectable()
@@ -12,7 +11,6 @@ export class BankService {
     const sql = 'INSERT INTO bank (member_id) VALUES ($1)';
     try {
       await pool.query(sql, [member.pk]);
-      console.log("bank 테이블 저장");
       return true;
     } catch (error) {
       console.error('bank 테이블 에러:', error);
