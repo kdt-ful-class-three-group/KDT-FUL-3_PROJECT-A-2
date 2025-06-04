a4cd33432b149170643d2897ad2ff153b84f0cf0
김윤지 <yunw0117@gmail.com>
2025-06-04 15:42:51 +0900

    Feat: banck db connect

--- nest/src/bank/bank.controller.ts
+++ nest/src/bank/bank.controller.ts
@@ -6,6 +6,11 @@ export class BankController {
   constructor(private readonly bankService: BankService) {}
 
   @Get()
+  async findAll() {
+    return this.bankService.findAll();
+  }
+
+  @Get('hello')
   getHello(): string {
     return this.bankService.getHello();
   }
--- nest/src/bank/bank.service.ts
+++ nest/src/bank/bank.service.ts
@@ -17,4 +17,9 @@ export class BankService {
       return false;
     }
   }
+
+  async findAll() {
+    const result = await pool.query('SELECT * FROM bank ORDER BY id DESC');
+    return result.rows;
+  }
 }
