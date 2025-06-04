a1d41af5f09b81695ae21422edfb36554991c90f
윤종환 <yunjonghwan99@gmail.com>
2025-06-04 10:46:36 +0900

    Chore: 불필요한 휴대폰 번호 입력 관련 코드 주석 처리

--- client/src/app/searchId/page.tsx
+++ client/src/app/searchId/page.tsx
@@ -35,35 +35,6 @@ export default function SearchId() {
           onRequestCode={handleEmail}
           isEmailCodeMatch={isEmailCodeMatch}
         />
-        {/* <div className="flex flex-col w-full max-w-xs mt-5">
-          <label className="text-[#FC4F00] mb-3">휴대폰 번호</label>
-          <div className="flex w-full justify-between mb-3">
-            <Input
-              className="pl-2 rounded-lg border py-2"
-              type="text"
-              placeholder="휴대폰 번호"
-              name="phone"
-              value={phone}
-              onChange={handlePhoneChange}
-            />
-            <button
-              className="bg-[#E5E5E5] text-[#1E3E62] rounded-lg"
-              type="button"
-            >
-              인증번호 받기
-            </button>
-          </div>
-          <div>
-            <Input
-              className="pl-2 rounded-lg border py-2 w-full mb-3"
-              type="text"
-              placeholder="인증번호 입력"
-              name="code"
-              value={code}
-              onChange={handleCodeChange}
-            />
-          </div>
-        </div> */}
         <button
           type="button"
           className="w-full max-w-xs mt-5 border-1 text-center bg-[#1E3E62] text-[#FFFFFF] p-2 rounded-lg"
